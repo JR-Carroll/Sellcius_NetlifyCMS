daff852afc12eb0e518c3c8fe0a07527ca7169d5
jcarroll <jrc.csus@gmail.com>
2020-09-08 11:20:52 -0600

    trying to upload changed components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,8 +41,8 @@ const Navbar = class extends React.Component {
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <Link to="/" className="navbar-item sellcius-logo-lg" title="Logo">
+              Sellcius Networks
             </Link>
             {/* Hamburger menu */}
             <div
@@ -77,16 +77,7 @@ const Navbar = class extends React.Component {
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
+              {/* Purposefully empty */}
             </div>
           </div>
         </div>
